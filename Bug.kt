5e4b7255c307badb314a0e329cf1d3f6119f6f74
BugHunter-X <askzenai@gmail.com>
2025-02-05 01:26:05 +0100

    Add Bug.kt file

---
+++ Bug.kt
@@ -0,0 +1,16 @@
+```kotlin
+fun main() {
+    val list = listOf(1, 2, 3, 4, 5)
+    val result = list.map { it * 2 }.filter { it > 5 }
+    println(result) // Output: [6, 8, 10]
+
+    val emptyList = emptyList<Int>()
+    val emptyResult = emptyList.map { it * 2 }.filter { it > 5 }
+    println(emptyResult) // Output: []
+
+    //This is incorrect
+    val nullableList: List<Int>? = null
+    val nullableResult = nullableList?.map { it * 2 }?.filter { it > 5 }
+    println(nullableResult) // Output: null
+}
+```
